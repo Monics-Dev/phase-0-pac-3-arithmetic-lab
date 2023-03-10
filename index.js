92ebdd540c679eb48892d54bf92dd30394219535
Monica <monijerobon@gmail.com>
2023-03-10 12:31:14 +0300

    Done

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function add(a,b) {
+    return a+b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function increment(n) {
+    return n+1;
+}
+function decrement(n) {
+    return n-1;
+}
+let n = 100;
+function makeInt(n) {
+    return parseInt(n,10);
+}
+makeInt();
+function   preserveDecimal(n){
+    return parseFloat(n);
+}
